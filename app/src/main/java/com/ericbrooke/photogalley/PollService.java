5f6be5e5188446e48a16eb1d99d1685462cf862d
Eric Brooke <ericbrooke@mac.com>
2016-06-23 17:32:57 -0400

    AlarmManager setup

--- app/src/main/java/com/ericbrooke/photogalley/PollService.java
+++ app/src/main/java/com/ericbrooke/photogalley/PollService.java
@@ -1,9 +1,12 @@
 package com.ericbrooke.photogalley;
 
+import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.SystemClock;
 import android.util.Log;
 
 import java.util.List;
@@ -14,10 +17,26 @@ import java.util.List;
 public class PollService extends IntentService {
     private static final String TAG = "PollService";
 
+    private static final int POLL_INTERVAL = 1000 * 60;
+
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
     }
 
+    public static void setServiceAlarm(Context context, boolean isOn) {
+        Intent i = PollService.newIntent(context);
+        PendingIntent pi = PendingIntent.getService(context, 0, i, 0);
+
+        AlarmManager alarmManager = (AlarmManager)
+                context.getSystemService(context.ALARM_SERVICE);
+        if (isOn) {
+            alarmManager.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
+                    SystemClock.elapsedRealtime(), POLL_INTERVAL, pi);
+        } else {
+            alarmManager.cancel(pi);
+            pi.cancel();
+        }
+    }
     public PollService() {
         super(TAG);
     }
@@ -27,11 +46,12 @@ public class PollService extends IntentService {
         if (!isNetworkAvailableAndConnected()) {
             return;
         }
-    String query = QueryPreferences.getStoredQuery(this);
-    String lastResultId = QueryPreferences.getLastResultId(this);
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+
         List<GalleryItem> items;
 
-        if (query ==null ) {
+        if (query == null ) {
             items = new FlickrFetchr().fetchRecentPhotos();
         } else {
             items = new FlickrFetchr().searchPhotos(query);
