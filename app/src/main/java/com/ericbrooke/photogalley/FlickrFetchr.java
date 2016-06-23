58b528450c7d88b30325b57cce83d5d859886196
Eric Brooke <ericbrooke@mac.com>
2016-06-23 10:29:48 -0400

    Basic fragment setup and basic networking

---
+++ app/src/main/java/com/ericbrooke/photogalley/FlickrFetchr.java
@@ -0,0 +1,44 @@
+package com.ericbrooke.photogalley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * Created by ebrooke on 6/23/16.
+ */
+public class FlickrFetchr {
+    public byte[] getURLBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            InputStream in = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() +
+                ": with " +
+                urlSpec);
+            }
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+            while ((bytesRead = in.read(buffer)) > 0) {
+                out.write(buffer, 0, bytesRead);
+            }
+            out.close();
+            return out.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getURLBytes(urlSpec));
+    }
+}
